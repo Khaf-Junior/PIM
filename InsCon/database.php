37ef597945623b3cc4b52ef658ce554962a28f49
ibrahima-4 <papeibrahimambaye888@gmail.com>
2025-03-10 15:18:15 +0000

    Avancement

--- InsCon/database.php
+++ InsCon/database.php
@@ -1,15 +1,15 @@
 <?php
 
 $host = 'localhost';  
-$dbname = ''; 
+$dbname = 'InsCon1'; 
 $username = 'root'; 
 $password = ''; 
 
 try {
 
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     echo "Connexion réussie avec PDO !";
 } catch (PDOException $e) {
