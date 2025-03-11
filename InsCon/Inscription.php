c85785399fd7a3a4b7d161cfeb9730eb946b7cf2
ibrahima-4 <papeibrahimambaye888@gmail.com>
2025-03-11 16:51:45 +0000

    ?

---
+++ InsCon/Inscription.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<style>
+    .form{
+        margin-top: 100px;
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+    }
+    h1{
+       text-align: center;
+       color: rgb(31, 245, 31);
+    }
+    
+</style>
+<body>
+    <h1>Bienvenue dans la page d'Inscription</h1>
+    <div class="form">
+        <h2>Inscrivez vous !</h2>
+        <form action="traitement.php" method="POST">
+            
+            <label for="prenom">Prenom</label>
+            <input type="text" name="prenom" id="prenom" required placeholder="Entrez votre prenom...">
+            <br><br>
+
+            <label for="prenom">Nom</label>
+            <input type="text" name="nom" id="nom" required placeholder="Entrez votre nom...">
+            <br><br>
+
+            <label for="prenom">Pseudo</label>
+            <input type="text" name="pseudo" id="pseudo" required placeholder="Entrez votre pseudo...">
+            <br><br>
+
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" required placeholder="Entrez votre email...">
+            <br><br>
+            
+            <label for="mot_de_passe">Mot de passe</label>
+            <input type="password" name="mdp" id="mdp" required placeholder="Entrez votre mot de passe...">
+            <br><br>
+
+            <input type="submit" name="ok" id="ok">
+        </form>
+    </div>
+</body>
+</html>
---
+++ InsCon/connexion.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+<style>
+    .form{
+        margin-top: 100px;
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+<body>
+
+    <?php 
+        include './database.php';
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $email = $_POST['email'];
+            $mdp = $_POST['mdp'];
+
+            if($email != "" && $mdp != ""){
+                $sql="SELECT * FROM utilisateurs WHERE Email='$email' AND MotDePasse = '$mdp' ";
+                $requete=$bdd->query($sql);
+                $rep=$requete->fetch();
+                if(isset($rep) && $rep != FALSE){
+                    header("Location: index.html");
+                    exit();
+                }else{
+                    $error_message = "Email ou mot de passe incorrect !";
+                }
+            }
+       }
+
+    ?>
+    <div class="form">
+        <form action="" method="POST">
+        <?php 
+            if(isset($_POST['ok'])){
+                if($error_message){
+            ?><p style="color: red;"><?php echo $error_message  ?></p>
+              <?php    
+              }
+            }
+        ?>
+        <label for="email">Email</label>
+            <input type="email" name="email" id="email" required placeholder="Entrez votre email...">
+            <br><br>
+            
+            <label for="mot_de_passe">Mot de passe</label>
+            <input type="password" name="mdp" id="mdp" required placeholder="Entrez votre mot de passe...">
+            <br><br>
+
+            <input type="submit" name="ok" id="ok" value="Se connecter">
+        </form>
+    </div>
+</body>
+</html>
